641c158be93403e076dcf71b8803a3388a75e31b
rozen03 <urielrozenberg@hotmail.com>
2018-06-04 09:23:09 -0300

    got it working

--- node.cpp
+++ node.cpp
@@ -10,20 +10,25 @@
 #include <map>
 #include <unistd.h>
 #include <mutex>
+#include <random>
 int total_nodes, mpi_rank;
-Block *last_block_in_chain;
 // map<string,Block> node_blocks;
 atomic<bool> probando;
-queue<Block*> waiting;
+queue<Block> waiting;
+std::mt19937 gen;
+int contador;
 
-//Envia el bloque minado a todos los nodos
-void broadcast_block(const Block *block){
-    //No enviar a mí mismo, desde el de la derecha en adelante
-    int dest;
-    for (int i =1; i <total_nodes; i++) {
-        dest = (mpi_rank+i)%total_nodes;
-        MPI_Send(block, 1, *MPI_BLOCK, dest, TAG_NEW_BLOCK, MPI_COMM_WORLD);
-    }
+int  getRandom(int a){
+	int res = (gen()+mpi_rank+1)%total_nodes;
+	while(res==a){
+		res = (gen()+mpi_rank+1)%total_nodes;
+	}
+	return res;
+}
+
+void send_block(const Block *block){
+    int dest= getRandom(mpi_rank);
+	MPI_Send(block, 1, *MPI_BLOCK, dest, TAG_NEW_BLOCK, MPI_COMM_WORLD);
 }
 
 void lock(){
@@ -38,45 +43,58 @@ void unlock(){
 }
 
 //Proof of work
-void* proof_of_work(void *ptr){
-    string hash_hex_str;
-    Block block;
-    unsigned int mined_blocks = 0;
-    while(true){
-		lock();
-        block = *last_block_in_chain;
-		unlock();
-        //Preparar nuevo bloque
-        block.index += 1;
-        block.node_owner_number = mpi_rank;
-        block.created_at = static_cast<unsigned long int> (time(NULL));
-		block.arrived_at = static_cast<unsigned long int> (time(NULL));
-        //Contar la cantidad de ceros iniciales (con el nuevo nonce)
-        if(solves_problem(hash_hex_str)){
-            //Verifico que no haya cambiado mientras calculaba
-            lock();
-            if(last_block_in_chain->index < block.index){
-                mined_blocks += 1;
-                *last_block_in_chain = block;
-                // node_blocks[hash_hex_str] = *last_block_in_chain;
-                printf("[%d] Agregué un producido con index %d \n",mpi_rank,last_block_in_chain->index);
-                //Mientras comunico, no responder mensajes de nuevos nodos
-                broadcast_block(last_block_in_chain);
-				sleep(1);
-            }
-            unlock();
-        }
+void* create_blocks(void *ptr){
 
-		// if (last_block_in_chain->index  >= MAX_BLOCKS){
+    string hash_hex_str;
+	int index =0;
+    while(true){
+		index++;
+		Block* block = new Block;
+        //Preparar nuevo bloque
+        block->index = index;
+        block->node_owner_number = mpi_rank;
+		block->node_destination_number =getRandom(mpi_rank);
+        block->created_at = static_cast<unsigned long int> (time(NULL));
+		block->arrived_at = static_cast<unsigned long int> (time(NULL));
+		// printf("[%d] Agregué un producido con index %d \n",mpi_rank,last_block_in_chain->index);
+		// node_blocks[hash_hex_str] = *last_block_in_chain;
+		lock();
+		waiting.push(*block);
+		unlock();
+		sleep(0.05);
+		if (index  >= MAX_BLOCKS){
 			// MPI_Abort(MPI_COMM_WORLD,0);
-			// break;
-		// }
+			break;
+		}
     }
+	printf("[%d] listo los noodo \n",mpi_rank);
     return NULL;
 }
+void* send_blocks(void *ptr){
+	Block block;
+	while(true){
+		lock();
+		if(!waiting.empty()){
+			block =waiting.front();
+			waiting.pop();
+			unlock();
+			send_block(&block);
+			// printf("[%d] mando bauhura \n",mpi_rank);
+			// delete block;
 
+		}else{
+			unlock();
+			// printf("[%d] nada por aqui, nada por allá \n",mpi_rank);
+			sleep(0.5);
+		}
+	}
+}
 
 int node(){
+	contador=0;
+	int taim =time(0);
+	seed_seq sseq{taim,mpi_rank,total_nodes};
+	gen.seed(sseq);
     probando = false;
     //Tomar valor de mpi_rank y de nodos totales
     MPI_Comm_size(MPI_COMM_WORLD, &total_nodes);
@@ -85,31 +103,33 @@ int node(){
     srand(time(NULL) + mpi_rank);
     printf("[MPI] Lanzando proceso %u\n", mpi_rank);
 
-    last_block_in_chain = new Block;
-
-    //Inicializo el primer bloque
-    last_block_in_chain->index = 0;
-    last_block_in_chain->node_owner_number = mpi_rank;
-    last_block_in_chain->created_at = static_cast<unsigned long int> (time(NULL));
-
     //Crear thread para minar
-    pthread_t minero;
-    pthread_create(&minero, NULL, proof_of_work, NULL);
+    pthread_t creater;
+    pthread_create(&creater, NULL, create_blocks, NULL);
 
+	pthread_t sender;
+	pthread_create(&sender, NULL, send_blocks, NULL);
     char block_hash[HASH_SIZE];
 
 	MPI_Status status;
+	Block *block = new Block;
     while(true){
 		//Recibir mensajes de otros nodos
 		MPI_Probe(MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
         auto tag = status.MPI_TAG;
         if (tag == TAG_NEW_BLOCK) {
-			Block *block = new Block;
             MPI_Recv(block, 1, *MPI_BLOCK,  status.MPI_SOURCE, TAG_NEW_BLOCK, MPI_COMM_WORLD, &status);
-			waiting.push(block);
+			if(block->node_destination_number!=(uint)mpi_rank){
+				lock();
+				waiting.push(*block);
+				unlock();
+			}else{
+				printf("[%d] El bloque %d/%d llego a destino \n",mpi_rank,block->index,block->node_owner_number);
+				// delete block;
+			}
+			contador++;
             //Si es un mensaje de nuevo bloque, llamar a la función validate_block_for_chain con el bloque recibido y el estado de MPI
-			printf("[%d] recibi un bloquede [%d] \n",mpi_rank,block->node_owner_number);
-			printf("[%d] Mi largo  es %d \n",mpi_rank,waiting.size());
+			// printf("[%d] recibi %d un bloques \n",mpi_rank,contador);
 
         } else if(tag == TAG_CHAIN_HASH) {
             //Si es un mensaje de pedido de cadena, responderlo enviando los bloques correspondientes
@@ -121,7 +141,6 @@ int node(){
 	// myfile.open(to_string(mpi_rank)+".txt");
 	// myfile<<mpi_rank<<","<<vericaciones<<","<<fallidos<<endl;
 	// myfile.close();
-    delete last_block_in_chain;
     // delete block;
     return 0;
 }
--- node.h
+++ node.h
@@ -7,7 +7,7 @@
 #define TAG_NEW_BLOCK 10
 #define TAG_CHAIN_HASH 21
 #define TAG_CHAIN_RESPONSE 22
-#define MAX_BLOCKS 200
+#define MAX_BLOCKS 4000
 
 extern MPI_Datatype* MPI_BLOCK;
 
